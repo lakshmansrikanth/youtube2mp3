3096f180b60658d807ae4a2d41aed3480a5cd157
root <root@lakshman-VPCEB14EN.(none)>
2014-03-06 16:11:41 +0530

    adding comments

--- yt.php
+++ yt.php
@@ -18,24 +18,20 @@ if ( ! empty($xml->channel->item[0]->pubDate) ) {
 	$d = date("Y-m-d",strtotime($d)); //get in YMD format
 }
 
+//Assuming you are running this script everyday! It will check if any youtube uploads are done for today
 $today = date("Y-m-d");
-//$today = "2013-12-30";
-//$d = "2013-12-30";
+
 if ($d == $today) {
-	
 	/* Refer http://rg3.github.io/youtube-dl/  for complete details of this application*/
 	
 	/* sudo pip install --upgrade youtube_dl  or download the youtube-dl binary from the above url and place it in cwd*/
-	
-	$cmd = 'youtube-dl -o "/TEMP/DIRECTORY/%(title)s.%(ext)s" --restrict-filenames http://www.youtube.com/watch?v='.$id;
-	
+	$cmd = 'youtube-dl -o "/TEMP/DIRECTORY/%(title)s.%(ext)s" --restrict-filenames http://www.youtube.com/watch?v='.$id; //change the dir
 	exec($cmd);
-	$cmd2 = 'youtube-dl --get-filename -o "%(title)s.%(ext)s" --restrict-filenames http://www.youtube.com/watch?v='.$id;
+	$cmd2 = 'youtube-dl --get-filename -o "%(title)s.%(ext)s" --restrict-filenames http://www.youtube.com/watch?v='.$id; 
 	$s = exec($cmd2);
 	$file = basename($s,".mp4");
 	$file = $file.".mp3";
-	//echo $file;
-	exec('ffmpeg -i source/directory/'.$s.' -ab 32k -y /destination/directory/'.$file);
+	exec('ffmpeg -i /source/directory/'.$s.' -ab 32k -y /destination/directory/'.$file); //change the path accordingly
 } else {
 	//echo "No Hangout-on-air today";
 }
